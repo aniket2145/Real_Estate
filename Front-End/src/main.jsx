9c13c50887d2816b03f20baf17b602692594502b
aniket2145 <anikett2145@gmail.com>
2024-02-21 23:44:12 +0530

    modified main.jsx

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 //import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
-import { persistor, store } from "./Redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
