60d360d34926af52e598d5941f47809fa6e907c1
aniket2145 <anikett2145@gmail.com>
2024-02-23 10:40:21 +0530

    updated home page

--- Front-End/src/Pages/Home.jsx
+++ Front-End/src/Pages/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
-        const res = await fetch("/api/listing/get?offer=true&limit=4");
+        const res = await fetch("/api/listing/get?offer=true&limit=9");
         const data = await res.json();
         setOfferListings(data);
         fetchRentListings();
@@ -30,7 +30,7 @@ export default function Home() {
 
     const fetchRentListings = async () => {
       try {
-        const res = await fetch("api/listing/get?type=rent&limit=4");
+        const res = await fetch("api/listing/get?type=rent&limit=9");
         const data = await res.json();
         setRentListing(data);
         fetchSaleListings();
@@ -41,7 +41,7 @@ export default function Home() {
 
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch("api/listing/get?type=sell&limit=4");
+        const res = await fetch("api/listing/get?type=sell&limit=9");
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
